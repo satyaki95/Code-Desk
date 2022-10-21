0b8b541dfed9662afb92c288496cfeceeef737d4
satyaki95 <satyaki95@outlook.com>
2022-10-21 17:58:26 +0530

    set up react-router and basic code editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ModalProvider from "./context/ModalContext";
+import PlaygroundProvider from "./context/PlaygroundContext";
+import HomeScreen from "./screens/HomeScreen";
+import Playground from "./screens/Playground";
+import GlobalStyles from "./styles/global";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Page404 from "./screens/Page404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PlaygroundProvider>
+      <ModalProvider>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+
+            <Route
+              path='/code/:folderId/:playgroundId'
+              element={<Playground />}
+            />
+
+            {/* For undefined paths - 404 page */}
+            <Route path='*' element={<Page404 />} />
+          </Routes>
+        </BrowserRouter>
+      </ModalProvider>
+    </PlaygroundProvider>
   );
 }
 
