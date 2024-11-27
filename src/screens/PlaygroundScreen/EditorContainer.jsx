a3955a880a7cabafcd50973ca5a19f3c038cd2f5
satyaki95 <www.satyaki95@gmail.com>
2024-11-27 15:15:52 +0530

    chack

--- src/screens/PlaygroundScreen/EditorContainer.jsx
+++ src/screens/PlaygroundScreen/EditorContainer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./editorContainer.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -9,14 +9,64 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { Editor } from "@monaco-editor/react";
 
-
-
 const editorOptions = {
   fontSize: 16,
-  theme: "vs-dark"
-}
+  wordWrap: "on",
+};
 
-const EditorContainer = () => {
+const fileExtensionMaping = {
+  cpp: "cpp",
+  javascript: "js",
+  python: "py",
+  java: "java",
+};
+
+const EditorContainer = (fileId, folderId) => {
+  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("cpp");
+  const [theme, setTheme] = useState("vs-dark");
+  const codeRef = useRef();
+  const onChangeCode = (newCode) => {
+    codeRef.current = newCode;
+  };
+
+  const importCode = (event) => {
+    const file = event.target.files[0];
+    const fileType = file.type.includes("text");
+    if (fileType) {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+      fileReader.onload = (value) => {
+        const importedCode = value.target.result;
+        setCode(importedCode);
+      };
+    } else {
+      alert("Please choose a program file");
+    }
+  };
+
+  const exportCode = () => {
+    const codeValue = codeRef.current?.trim();
+
+    if (!codeValue) {
+      alert(`Please Type Some Code`);
+    } else {
+      const codeBlob = new Blob([codeValue], { type: "text/plain" });
+      const downloadUrl = URL.createObjectURL(codeBlob);
+      const link = document.createElement("a");
+      link.href = downloadUrl;
+      link.download = `code.${fileExtensionMaping[language]}`;
+      link.click();
+    }
+  };
+
+  const onChangeLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
+
+  const onChangeTheme = (e) => {
+    setTheme(e.target.value);
+  };
   return (
     <div className="root-editor-container">
       <div className="editor-header">
@@ -26,14 +76,14 @@ const EditorContainer = () => {
           <button>Save Code</button>
         </div>
         <div className="editor-right-container">
-          <select name="language">
+          <select onChange={onChangeLanguage} value={language}>
             <option value="cpp">CPP</option>
             <option value="java">Java</option>
             <option value="python">Python</option>
             <option value="javascript">JavaScript</option>
           </select>
 
-          <select>
+          <select onChange={onChangeTheme} value={theme}>
             <option value="vs-dark">Dark</option>
             <option value="vs-light">Light</option>
           </select>
@@ -42,8 +92,11 @@ const EditorContainer = () => {
       <div className="editor-body">
         <Editor
           height={"100%"}
-          language={"javascript"}
+          language={language}
           options={editorOptions}
+          theme={theme}
+          value={code}
+          onChange={onChangeCode}
         />
       </div>
       <div className="editor-footer">
@@ -62,8 +115,8 @@ const EditorContainer = () => {
           />
           <span>Import Code</span>
         </label>
-        <input type="file" id="import-code" />
-        <button>
+        <input type="file" id="import-code" onChange={importCode} />
+        <button onClick={exportCode}>
           <FontAwesomeIcon
             icon={faCloudArrowDown}
             className="export_code"
--- src/screens/PlaygroundScreen/PlaygroundScreen.jsx
+++ src/screens/PlaygroundScreen/PlaygroundScreen.jsx
@@ -7,9 +7,35 @@ import {
   faCloudArrowUp,
 } from "@fortawesome/free-solid-svg-icons";
 import EditorContainer from "./EditorContainer";
+import { useState } from "react";
+
 const PlaygroundScreen = () => {
   const params = useParams();
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
   const { fileId, folderId } = params;
+
+  const importInput = (e) => {
+    const file = e.target.files[0];
+    const fileType = file.type.includes("text");
+    if (fileType) {
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (event) => {
+        setInput(event.target.result);
+      };
+    }
+  };
+
+  const exportOutput = () => {
+    const blob = new Blob([output], {
+      type: "text/plain",
+    });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "output.txt";
+    link.click();
+  };
 
   return (
     <div className="playground-container">
@@ -23,7 +49,7 @@ const PlaygroundScreen = () => {
       </div>
       <div className="content-container">
         <div className="editor-container">
-          <EditorContainer />
+          <EditorContainer fileId={fileId} folderId={folderId} />
         </div>
         <div className="input-output-container">
           <div className="input-output-header">
@@ -36,14 +62,17 @@ const PlaygroundScreen = () => {
               />
               <span className=""> Import input</span>
             </label>
-            <input type="file" id="input" />
+            <input type="file" id="input" onChange={importInput} />
           </div>
-          <textarea></textarea>
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></textarea>
         </div>
         <div className="input-output-container">
           <div className="input-output-header">
             <b>Output :</b>
-            <button className="icon-container">
+            <button className="icon-container" onClick={exportOutput}>
               <FontAwesomeIcon
                 icon={faCloudArrowDown}
                 className="export_file"
@@ -52,7 +81,7 @@ const PlaygroundScreen = () => {
               <span className=""> Export Output</span>
             </button>
           </div>
-          <textarea readOnly></textarea>
+          <textarea readOnly value={output}></textarea>
         </div>
       </div>
     </div>
